9314cd107846dbd9e45737f32634ad00976ca973
Conanap <albionapc@gmail.com>
2019-03-01 16:06:19 -0500

    Initial Commit

---
+++ kb4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kb4
+//
+//  Created by Albion Ka Hei Fung on 2019-03-01.
+//  Copyright © 2019 Albion Ka Hei Fung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
