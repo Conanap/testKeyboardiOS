282337d779d8775f6e3ced04faf0ba3d29770d02
Conanap <albionapc@gmail.com>
2019-03-01 23:25:33 -0500

    init commit

---
+++ ckb4/KeyboardViewController.swift
@@ -0,0 +1,67 @@
+//
+//  KeyboardViewController.swift
+//  ckb4
+//
+//  Created by Albion Ka Hei Fung on 2019-03-01.
+//  Copyright © 2019 Albion Ka Hei Fung. All rights reserved.
+//
+
+import UIKit
+
+class KeyboardViewController: UIInputViewController {
+    var shift: Bool = false
+
+    @IBOutlet var nextKeyboardButton: UIButton!
+    
+    override func updateViewConstraints() {
+        super.updateViewConstraints()
+        
+        // Add custom view sizing constraints here
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let nib = UINib(nibName: "kbView", bundle: nil)
+        let objects = nib.instantiate(withOwner: self, options: nil)
+        
+        view = objects[0] as? UIView
+
+        // Perform custom UI setup here
+        self.nextKeyboardButton = UIButton(type: .system)
+        
+        self.nextKeyboardButton.setTitle(NSLocalizedString("Next Keyboard", comment: "Title for 'Next Keyboard' button"), for: [])
+        self.nextKeyboardButton.sizeToFit()
+        self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
+        
+        self.view.addSubview(self.nextKeyboardButton)
+        
+        self.nextKeyboardButton.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        self.nextKeyboardButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+    }
+    
+    @IBAction func buttonPressed(button: UIButton) {
+        let string = button.titleLabel?.text
+        (textDocumentProxy as UIKeyInput).insertText("\(string!)")
+    }
+    
+    override func textWillChange(_ textInput: UITextInput?) {
+        // The app is about to change the document's contents. Perform any preparation here.
+    }
+    
+    override func textDidChange(_ textInput: UITextInput?) {
+        // The app has just changed the document's contents, the document context has been updated.
+        
+        var textColor: UIColor
+        let proxy = self.textDocumentProxy
+        if proxy.keyboardAppearance == UIKeyboardAppearance.dark {
+            textColor = UIColor.white
+        } else {
+            textColor = UIColor.black
+        }
+        self.nextKeyboardButton.setTitleColor(textColor, for: [])
+    }
+
+}
